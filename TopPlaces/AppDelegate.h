1410b0ce3795a98076bbc178053387c2cf536de2
Angela Hsu <ayh123@gmail.com>
2015-08-26 10:50:42 -0500

    Initial Commit

---
+++ TopPlaces/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TopPlaces
+//
+//  Created by Angela Hsu on 8/26/15.
+//  Copyright (c) 2015 Optaros. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
